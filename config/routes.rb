8f6c8855009fdf82a87b48e67c8952cac7db148a
littlephillips <phillipkinuthia72@gmail.com>
2022-11-21 12:17:15 +0300

    rails routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # http verb:get, path:cheeses, controller action:cheeses#index
+  get 'cheeses', to: 'cheeses#index'
+
 end
